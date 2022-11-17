709265ce1835b3703dbfba2c5f7b89605bb94739
2020nani <nanialmeida2019@gmail.com>
2022-11-16 22:12:36 -0300

    projeto inicial

---
+++ src/test/java/br/com/docesdalu/DocesdaluApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docesdalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocesdaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
